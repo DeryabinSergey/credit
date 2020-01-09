f1e46d304255bb2ddcde326f301a578256b54888
Дерябин Сергей <DeryabinSergey@gmail.com>
2020-01-09 23:17:00 +0300

    Косметика в интерфейсе

--- src/user/controllers/adminPanel.class.php
+++ src/user/controllers/adminPanel.class.php
@@ -7,6 +7,21 @@ class adminPanel extends baseFront implements UserController, SecurityController
         $model = parent::getModel($request);
         
         if ($this->isDisplayView()) {
+            
+            if (SecurityManager::isAllowedAction(AclAction::VIEW_ACTION, AclContext::INVESTOR_OFFER_ID)) {
+                $model->set('investorOfferNum', Criteria::create(InvestorOffer::dao())->addProjection(Projection::count('id', 'num'))->add(Expression::isFalse('deleted'))->getCustom('num'));
+            }
+            if (SecurityManager::isAllowedAction(AclAction::PUBLISH_ACTION, AclContext::INVESTOR_OFFER_ID)) {
+                $model->set('investorOfferModerate', Criteria::create(InvestorOffer::dao())->addProjection(Projection::count('id', 'num'))->add(Expression::isFalse('active'))->getCustom('num'));
+            }
+            
+            if (SecurityManager::isAllowedAction(AclAction::VIEW_ACTION, AclContext::CREDITOR_ID)) {
+                $model->set('creditorNum', Criteria::create(Creditor::dao())->addProjection(Projection::count('id', 'num'))->add(Expression::isFalse('deleted'))->getCustom('num'));
+            }
+            if (SecurityManager::isAllowedAction(AclAction::PUBLISH_ACTION, AclContext::CREDITOR_ID)) {
+                $model->set('creditorModerate', Criteria::create(Creditor::dao())->addProjection(Projection::count('id', 'num'))->add(Expression::isFalse('active'))->getCustom('num'));
+            }  
+            
         }
         return $model;
     }
