8e96c767fbb5be01225f24d805e308130261ef6f
Дерябин Сергей <DeryabinSergey@gmail.com>
2020-03-25 14:08:46 +0300

    Сделал добавление заявки одностраничным и без указания категории залога и типа заемщика

--- src/controllers/CreditRequest/CreditRequestStartCommand.class.php
+++ src/controllers/CreditRequest/CreditRequestStartCommand.class.php
@@ -8,7 +8,8 @@ class CreditRequestStartCommand implements EditorCommand
     const ERROR_AUTH_ENABLED    = 0x0006;
     
     const ERROR_EXPIRED         = 0x0003;
-
+    const ERROR_INVALID     = 0x0003;
+    const ERROR_YONG        = 0x0003;
     /**
      * @return CreditRequestStartCommand
      */
@@ -20,77 +21,176 @@ class CreditRequestStartCommand implements EditorCommand
         $mav = ModelAndView::create();
         
         $form->markGood('id');
-        $subject = $form->getValue('id');
-        $userExists = $confirm = null;
+        $subject = CreditRequest::create();
+        $userExists = SecurityManager::getUser();
+        $confirm = null;
         
         if (!SecurityManager::isAuth() && !SecurityManager::isAuthEnabled()) { $form->markCustom('phone', self::ERROR_AUTH_ENABLED); }
         
-        /** Отправляем код подтверждения, если чувак не додумался нажать на кнопку отправить код **/
-        if ($process && !$form->getError('phone') && $form->getValue('phone')) {    
-            try {
-                $confirm = Confirm::dao()->getByLogic(Expression::andBlock(Expression::eq('type_id', ConfirmType::TYPE_CREDIT_REQUEST), Expression::eq('phone', $form->getValue('phone'))));
-            } catch (ObjectNotFoundException $e) {
-                $confirm = 
-                    Confirm::dao()->add(
-                        Confirm::create()->
-                            setType(ConfirmType::create(ConfirmType::TYPE_CREDIT_REQUEST))->
-                            setPhone($form->getValue('phone'))->
-                            setCode(random_int(1, 9999))
-                    );
-                SmsUtils::send("7{$form->getValue('phone')}", sprintf("Код подтверждения для оформления заявки на кредит: %04d", $confirm->getCode()));
-            }            
+        
+        if ($process && !SecurityManager::isAuth()) {    
+            
+            /** Отправляем код подтверждения, если чувак не додумался нажать на кнопку отправить код **/
+            if (!$form->getError('phone') && $form->getValue('phone')) {
+                try {
+                    $confirm = Confirm::dao()->getByLogic(Expression::andBlock(Expression::eq('type_id', ConfirmType::TYPE_CREDIT_REQUEST), Expression::eq('phone', $form->getValue('phone'))));
+                } catch (ObjectNotFoundException $e) {
+                    $confirm = 
+                        Confirm::dao()->add(
+                            Confirm::create()->
+                                setType(ConfirmType::create(ConfirmType::TYPE_CREDIT_REQUEST))->
+                                setPhone($form->getValue('phone'))->
+                                setCode(random_int(1, 9999))
+                        );
+                    SmsUtils::send("7{$form->getValue('phone')}", sprintf("Код подтверждения для оформления заявки на кредит: %04d", $confirm->getCode()));
+                } 
+            }
+            
+            if (!$form->getValue('accept')) { 
+                $form->markWrong('accept');
+            } elseif ($form->getValue('code')) {
+
+                $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify", false, stream_context_create( array('http' => array('method' => 'POST', 'header' => 'Content-Type: application/x-www-form-urlencoded' . PHP_EOL, 'content' => http_build_query(array('secret' => GOOGLE_RECAPTCHA_CLOSED, 'response' => $form->getValue('response')))) ) )), "true");
+                if (!$response['success']) {
+                    $form->markWrong('response');
+                } else {
+
+                    try {
+                        if (Timestamp::compare($confirm->getExpiredTime(), Timestamp::makeNow()) == -1 || $confirm->getTry() >= 3) throw new ObjectNotFoundException();
+                        if ($confirm->getCode() != $form->getValue('code')) {
+                            $form->markWrong('code');
+                            $confirm->dao()->save($confirm->setTry($confirm->getTry() + 1));
+                        } else {
+                            try {
+                                $userExists = User::dao()->getByLogic(Expression::eq('phone', $form->getValue('phone')));
+                            } catch(ObjectNotFoundException $e) { 
+                                
+                                $password = ViewTextUtils::createRandomPassword(10);
+
+                                $userExists = 
+                                    User::dao()->
+                                        add(
+                                            User::create()->
+                                                setName($form->getValue('phone'))->
+                                                setPassword(hash('sha256', $password))->
+                                                setPhone($form->getValue('phone'))->
+                                                setActive(true)
+                                        );
+
+                                SmsUtils::send("7{$form->getValue('phone')}", "Ваш пароль для входа на сайт: {$password}");                                
+                                
+                            }
+                            
+                            if ($userExists->isBan()) {
+                                $form->markCustom('phone', self::ERROR_BAN);
+                            } else {
+                                SecurityManager::setUser($userExists, true, $request);
+                                $form->drop('phone')->drop('code')->drop('accept')->drop('response');
+                            }
+                            $confirm->dao()->dropById($confirm->getId());
+                        }
+
+                    } catch (ObjectNotFoundException $e) {
+                        $form->markCustom('code', self::ERROR_EXPIRED);
+                    }
+                }                
+            }
         }
         
         if ($process && !$form->getErrors()) {
-
-            if (!$form->getValue('accept')) { $form->markWrong('accept'); }
-            $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify", false, stream_context_create( array('http' => array('method' => 'POST', 'header' => 'Content-Type: application/x-www-form-urlencoded' . PHP_EOL, 'content' => http_build_query(array('secret' => GOOGLE_RECAPTCHA_CLOSED, 'response' => $form->getValue('response')))) ) )), "true");
-            if (!$response['success']) {
-                $form->markWrong('response');
-            } else {
-                try {
-                    $userExists = User::dao()->getByLogic(Expression::eq('phone', $form->getValue('phone')));
-                } catch(ObjectNotFoundException $e) { /* nothin here */ }
                 
-                try {
-                    if (
-                        Timestamp::compare($confirm->getExpiredTime(), Timestamp::makeNow()) == -1 ||
-                        $confirm->getTry() >= 3
-                    ) throw new ObjectNotFoundException();
-                    if ($confirm->getCode() != $form->getValue('code')) {
-                        $form->markWrong('code');
-                        $confirm->dao()->save($confirm->setTry($confirm->getTry() + 1));
-                    }
-                        
-                } catch (ObjectNotFoundException $e) {
-                    $form->markCustom('code', self::ERROR_EXPIRED);
+                if (!$form->getErrors() && Timestamp::compare($form->getValue('birthDate'), Timestamp::create("-18 year")) > 0) {
+                    $form->markCustom('birthDate', self::ERROR_YONG);
                 }
-            }
-            
-            if (!$form->getErrors()) {
                 
-                if (!$userExists instanceof User) {
-                    $password = ViewTextUtils::createRandomPassword(10);
-
-                    $userExists = 
-                        User::dao()->
-                            add(
-                                User::create()->
-                                    setName($form->getValue('phone'))->
-                                    setPassword(hash('sha256', $password))->
-                                    setPhone($form->getValue('phone'))->
-                                    setActive(true)
-                            );
-
-                    SmsUtils::send("7{$form->getValue('phone')}", "Ваш пароль для входа на сайт: {$password}");
-                    SecurityManager::setUser($userExists, true, $request);
-                } elseif ($userExists instanceof User && !SecurityManager::isAuth()) {
-                    SecurityManager::setUser($userExists, true, $request);
-                }            
-                $confirm->dao()->dropById($confirm->getId());
+                if (!$form->getErrors()) {
+                    $req = json_encode(array($form->getValue('passport')));
+                    $head = "Content-Type: application/json\r\nAccept: application/json\r\nAuthorization: Token ".Constants::DADATA_TOKEN."\r\nX-Secret: ".Constants::DADATA_SECRET . PHP_EOL;
+                    try {
+                        $response = json_decode(file_get_contents("https://cleaner.dadata.ru/api/v1/clean/passport", false, stream_context_create( array('http' => array('method' => 'POST', 'header' => $head, 'content' => $req) ) )), "true");
+                        if ($response && is_array($response) && isset($response[0]) && is_array($response[0]) && isset($response[0]['qc']) && $response[0]['qc'] != 0) {
+                            $form->markCustom('passport', self::ERROR_INVALID);
+                        }
+                    } catch(Exception $e) { 
+                        /**
+                         * @fixme
+                         * Здесть при ошибке или недоступности сервиса надо бы ставить отметку что данные паспорта не проверены.
+                         * Наверняка все долбоебы привыкнут и будут считать эти данные достоверными
+                         */
+                    }
+                }
                 
-                $mav->setView(EditorController::COMMAND_SUCCEEDED);
-            }
+                if (!$form->getErrors()) {
+                    
+                    FormUtils::form2object($form, $subject);
+                    $subject = $subject->dao()->add($subject->setUser($userExists)->setType(SubjectType::create(SubjectType::TYPE_FIZ)));
+                    
+                    /**
+                     * Если именем пользователя было установлен номер телефона на предыдущем шаге (заявка без регистрации) - 
+                     * пытаемся здесь получить имя пользователя
+                     */
+                    if (
+                        !$userExists->getEmail() && 
+                        $userExists->getName() == $userExists->getPhone() && 
+                        $subject->getName()
+                    ) {
+                        $userName = explode(" ", $subject->getName(), $subject->getType()->isIP() ? 3 : 2);
+                        if (
+                            ($subject->getType()->isIP() && count($userName) == 3 && $userName[2]) ||
+                            ($subject->getType()->isFiz() && count($userName) == 2 && $userName[1])
+                        ) {
+                            $userName = $userName[$subject->getType()->isIP() ? 2 : 1];
+                            $userExists->dao()->save($userExists->setName($userName));
+                        }
+                        
+                    }
+                    
+                    /**
+                     * Отправка уведомлений пользователям с правами на публикацию заявлений на кредит
+                     */
+                    $groupsIds = 
+                        ArrayUtils::convertToPlainList(
+                            Criteria::create(AclGroupRight::dao())->
+                                setDistinct()->
+                                addProjection(Projection::property('group'))->
+                                add(Expression::eq('right.context', AclContext::CREDIT_REQUEST_ID))->
+                                add(Expression::eq('right.action.action', AclAction::PUBLISH_ACTION))->
+                                getCustomList(), 'group_id'
+                        );
+                    
+                    if ($groupsIds) {
+                        $users = 
+                            Criteria::create(User::dao())->
+                                add(Expression::in('group', $groupsIds))->
+                                getList();
+                        
+                        $telegram = TelegramSender::create('creditRequestNew')->set('request', $subject);
+                        
+                        foreach($users as $user) {
+                            
+                            if ($user->getEmail()) {
+                                MimeMailSender::create('Новая заявка на кредит', 'creditRequestNewHtml', 'creditRequestNewText')->
+                                    setTo($user->getEmail(), $user->getName())->
+                                    set('request', $subject)->
+                                    set('user', $user)->
+                                    send();
+                            }
+                            
+                            if ($user->getTelegramId() && $user->isTelegramBotEnabled()) {
+                                try {
+                                    $telegram->send($user->getTelegramId());
+                                } catch(BaseException $e) {
+                                    if (mb_stripos($e->getMessage(), "403 Forbidden")) {
+                                        $user->dao()->save($user->setTelegramBotEnabled(false));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                    $mav->setView(EditorController::COMMAND_SUCCEEDED);
+                    
+                }
         }
 
         if ($mav->getView() != EditorController::COMMAND_SUCCEEDED) {
@@ -104,17 +204,25 @@ class CreditRequestStartCommand implements EditorCommand
 
     public function setForm(Form $form)
     {
-        $neededPrimitives = array('id', 'securityCode', 'action', 'go', 'return', 'cancel');
+        $neededPrimitives = array('id', 'securityCode', 'name', 'birthDate', 'action', 'go', 'return', 'cancel');
         foreach($form->getPrimitiveNames() as $primitive) {
             if (!in_array($primitive, $neededPrimitives)) {
                 $form->drop($primitive);
             }
         }
         
-        $form->add(Primitive::boolean('accept')->required());
-        $form->add(Primitive::string('phone')->addImportFilter(Filter::pcre()->setExpression("/([^\d]+)/", ""))->setAllowedPattern("/\d{10}/is")->required());
-        $form->add(Primitive::integer('code')->setMax(9999)->setMin(1)->required());
-        $form->add(Primitive::string('response')->required());
+        if (!SecurityManager::isAuth()) {
+            $form->add(Primitive::boolean('accept')->required());
+            $form->add(Primitive::string('phone')->addImportFilter(Filter::pcre()->setExpression("/([^\d]+)/", ""))->setAllowedPattern("/\d{10}/is")->required());
+            $form->add(Primitive::integer('code')->setMax(9999)->setMin(1)->required());
+            $form->add(Primitive::string('response')->required());
+        }
+        
+        $form->get('birthDate')->required();
+        $form->add(Primitive::string('summ')->addImportFilter(Filter::pcre()->setExpression("/([^\d]+)/isu", ""))->required());
+        $form->add(Primitive::string('passport')->setAllowedPattern("/^\d{10}/is")->addImportFilter(Filter::pcre()->setExpression("/([^\d]+)/isu", ""))->required());
+        $form->get('name')->addImportFilter(Filter::textImport())->addDisplayFilter(Filter::htmlSpecialChars())->required();
+        
         
         return $this;
     }
