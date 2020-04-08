26f8c087028ffefa543a60cd44f726b209f572f0
Дерябин Сергей <DeryabinSergey@gmail.com>
2020-04-08 05:26:37 +0300

    Изменил заголовок на SEO

--- src/user/controllers/main.class.php
+++ src/user/controllers/main.class.php
@@ -7,7 +7,7 @@ class main extends baseFront
         $model = parent::getModel($request);
         
         if ($this->isDisplayView()) {
-            Singleton::getInstance('HTMLMetaManager')->setTitle('Быстрые займы под залог');
+            Singleton::getInstance('HTMLMetaManager')->setTitle('Кредит под залог автомобиля - FinResurs');
         }
         
         return $model;
